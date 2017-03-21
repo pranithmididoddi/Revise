354e4df84e51bb1ee21bd8e4fd85a488068b1020
Srinivas <pranith13@gmail.com>
2017-03-21 11:52:41 -0500

    1.postorder 2.MinStack

---
+++ src/MinStack.java
@@ -0,0 +1,39 @@
+/**
+ * Created by Pranith on 3/21/17.
+ */
+import java.util.*;
+public class MinStack {
+
+        private Stack<Integer> stack;
+        private Stack<Integer> minStack;
+
+
+        public MinStack() {
+            stack=new Stack<Integer>();
+            minStack=new Stack<Integer>();
+        }
+
+        public void push(int x) {
+            stack.push(x);
+            if(minStack.empty()){
+                minStack.push(x);
+            }else{
+                minStack.push(Math.min(x, minStack.peek()));
+            }
+        }
+
+        public void pop() {
+            minStack.pop();
+            stack.pop();
+        }
+
+        public int top() {
+            return stack.peek();
+        }
+
+        public int getMin() {
+            return minStack.peek();
+        }
+    }
+
+}
